85b126b7115ab8dca958ebafbf4031a7736c5f09
nyccreator <andyfernandeznyc@gmail.com>
2023-11-20 03:38:27 -0500

    Edit Bash script to also terminate both processes

--- hoopcharts-api.sh
+++ hoopcharts-api.sh
@@ -3,6 +3,17 @@
 # Start Flask app
 gunicorn app:app &
 
-# Start ngrok
-ngrok http --domain=normal-dinosaur-yearly.ngrok-free.app 8000
+# Store the PID of the last background process (gunicorn)
+pid1=$!
 
+# Start ngrok
+ngrok http --region=us --hostname=normal-dinosaur-yearly.ngrok-free.app 8000 &
+
+# Store the PID of the second background process (ngrok)
+pid2=$!
+
+# Wait for the background process (ngrok) to finish
+wait $pid2
+
+# Terminate gunicorn process
+kill $pid1
